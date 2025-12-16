6a7bbf0d66c8e86fa13a2a39c06142283df682c3
froemailtest <128370484+froemailtest@users.noreply.github.com>
2025-12-16 10:17:26 +0530

    colour
    add background colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,28 @@ import CardList from "../components/ui/CardList";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex flex-col">
-
+    <div
+      className="
+        min-h-screen flex flex-col
+        bg-purple-300
+        sm:bg-pink-300
+        md:bg-red-300
+        lg:bg-green-300
+        xl:bg-gray-300
+        2xl:bg-purple-300
+      "
+    >
       <nav className="mb-6 bg-blue-200 p-4">
         <header className="text-center">
           <h1 className="text-2xl font-semibold text-black">
-            Product Card          </h1>
+            Product Card
+          </h1>
         </header>
       </nav>
 
-      {/* Helping text */}
       <p className="mb-4 text-center text-black">
-        Responsive Product card and changes layout      </p>
+        Responsive Product card and changes layout
+      </p>
 
       <main className="flex-1">
         <CardList />
@@ -22,7 +32,6 @@ export default function HomePage() {
       <footer className="mt-auto text-center text-sm text-gray-600 mb-2">
         Created By:[Rinkal Donga]
       </footer>
-
     </div>
   );
 }
