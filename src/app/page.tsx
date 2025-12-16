31a371b791bc3cd0c93fb61602fa9412b652fae6
froemailtest <128370484+froemailtest@users.noreply.github.com>
2025-12-16 09:44:19 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,23 @@ import CardList from "../components/ui/CardList";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6 flex flex-col">
+    <div className="min-h-screen">
 
-
-      <header className="mb-6 text-center">
-        <h1 className="text-2xl font-semibold">
-          Responsive Card Layout for Products
-        </h1>
-      </header>
+      <nav className="mb-6 bg-blue-200 p-4 ">
+        <header className="text-center">
+          <h1 className="text-2xl font-semibold text-black">
+            Responsive Card Layout for Products
+          </h1>
+        </header>
+      </nav>
 
       <main className="flex-1">
         <CardList />
       </main>
 
-      <footer className="mt-6 text-center text-sm text-gray-600">
-        Created By:[Rinkal Donga]      </footer>
+      <footer className="mt-6 text-center text-sm text-gray-600 mb-2">
+        Created By:[Rinkal Donga]
+      </footer>
 
     </div>
   );
