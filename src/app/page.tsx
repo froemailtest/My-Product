b583c34ca27cd9a4473cfdbf656b12344eafe218
froemailtest <128370484+froemailtest@users.noreply.github.com>
2025-12-16 10:08:40 +0530

    add footer
    Add footer proper and add helping text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,24 @@ import CardList from "../components/ui/CardList";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col">
 
-      <nav className="mb-6 bg-blue-200 p-4 ">
+      <nav className="mb-6 bg-blue-200 p-4">
         <header className="text-center">
           <h1 className="text-2xl font-semibold text-black">
-            Responsive Card Layout for Products
-          </h1>
+            Product Card          </h1>
         </header>
       </nav>
+
+      {/* Helping text */}
+      <p className="mb-4 text-center text-black">
+        Responsive Product card and changes layout      </p>
 
       <main className="flex-1">
         <CardList />
       </main>
 
-      <footer className="mt-6 text-center text-sm text-gray-600 mb-2">
+      <footer className="mt-auto text-center text-sm text-gray-600 mb-2">
         Created By:[Rinkal Donga]
       </footer>
 
